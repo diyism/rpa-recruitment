c2ff17d4627f6f7a72c7145bce6f12b46f6816ee
Malcolm <kexianbin@diyism.com>
2021-03-11 10:37:39 +0800

    add runkit mock_date for test

--- php/lib/MyGreeter.php
+++ php/lib/MyGreeter.php
@@ -22,7 +22,3 @@ class Client
          }
     }
 }
-
-
-//$greeter = new \MyGreeter\Client();
-//echo $greeter->getGreeting();
--- php/test/MyGreeter_Client_Test.php
+++ php/test/MyGreeter_Client_Test.php
@@ -23,4 +23,10 @@ class MyGreeter_Client_Test extends PHPUnit\Framework\TestCase
             strlen($this->greeter->getGreeting()) > 0
         );
     }
+
+    private function mock_date($mock_datetime)
+    {
+        runkit_function_rename('date', 'date_real');
+        runkit_function_add('date','$format="Y-m-d H:i:s", $timestamp=NULL', '$ts = $timestamp ? $timestamp : strtotime('.$mock_datetime.'); return date_real($format, $ts);');
+    }
 }
