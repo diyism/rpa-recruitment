76484e05396b07b614d3856c37cee35f067caa53
Malcolm <kexianbin@diyism.com>
2021-03-11 09:33:55 +0800

    finish the MyGreeter\Client class

--- php/MyGreeter.php
+++ php/MyGreeter.php
@@ -1,14 +1,28 @@
 <?php
 namespace MyGreeter;
 
+date_default_timezone_set('Etc/GMT-8');
+
 class Client
 {
     function getGreeting()
     {
-         echo 'greet';
+         $h=date('H');
+         if ($h>='00' and $h<'12')
+         {
+            return 'Good morning';
+         }
+         else if ($h>='12' and $h<'18')
+         {
+            return 'Good afternoon';
+         }
+         else
+         {
+            return 'Good evening';
+         }
     }
 }
 
 
-$greeter = new \MyGreeter\Client();
-$greeter->getGreeting();
+//$greeter = new \MyGreeter\Client();
+//echo $greeter->getGreeting();
